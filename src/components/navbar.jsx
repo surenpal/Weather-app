303169c30f572ba0d7e61415737ab8830f4bec1d
suren <surenpal666@gmail.com>
2026-01-02 14:43:09 +0900

    I am commiting the Navbar.jsx file after resolvint the git global OS: troubleshoot

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const navbar = () => {
+  return (
+    <div>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+    </div>
+  )
+}
