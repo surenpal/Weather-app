365a7227ea5c2c28ceedf926c1b44e83c82bfada
suren <surenpal666@gmail.com>
2026-01-02 22:40:16 +0900

    weather app title isadded

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react"
+import './index.css'
 
-export const App = () => {
+function App () {
+
+
   return (
-    <div>Creating weather app in cold</div>
+    <div className = "min-h-screen flex flex-col items-center justify-center bg-blue-100" >
+      <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full"><h1 className = "text-3xl font-bold text-center">Weather-App</h1></div>
+    </div>
   )
 }
+
+export default App
