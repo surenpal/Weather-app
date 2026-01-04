7e746768628f3ce07ef37408be904f1c6dde6086
suren <surenpal666@gmail.com>
2026-01-05 00:19:22 +0900

    humidity and the wind speed is added and the result is also showing on the same time

--- src/components/weatherCard.jsx
+++ src/components/weatherCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const WeatherCard = ({ weather }) => {
   return (
-    <div className='mt-6'>
+        <div className='mt-6'>
         <h2 className='text-2xl font-semi-cold text-center'>{weather.name}, {weather.sys.country}</h2>
         <div className=' flex justify-center items-center mt-4'>
             <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} 
@@ -10,11 +10,21 @@ export const WeatherCard = ({ weather }) => {
             <p className=' 4xl font-bold'>
                 {Math.round(weather.main.temp)}°C
             </p>
+        </div>
 
             <p className='text-center text-gray-400 capitalize'> 
                 {weather.weather[0].description}
             </p>
+            <div>
+                <div>
+                    <p>Humidity</p>
+                    <p>{weather.main.humidity}%</p>
+                </div>
+                 <div>
+                    <p>Wind speed</p>
+                    <p>{weather.wind.speed} m/s</p>
+                </div>
+            </div>
         </div>
-    </div>
   )
 }
