ecd42c45136aef2c3918865178a9c19fbb88ae9b
suren <surenpal666@gmail.com>
2026-01-04 18:18:57 +0900

    css for the input box is added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 
 export const SearchBar = () => {
-  return (
 
-        const [city, setCity] = useState ("")
+    const [city, setCity] = useState ("")
+
+  return (
     <form>
-        <input type="text" placeholder='Enter city name' />
+        <input type="text" placeholder='Enter city name' value={city}
+        onChange={(e) => setCity = (e.target.value)}
+        className='p-2 border border-gray-300 rounded-lg outline none'/>
     </form>
   )
 }
