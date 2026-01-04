45dbd4dfae20d40124dc3de33073fbb9c959bc53
suren <surenpal666@gmail.com>
2026-01-04 19:09:45 +0900

    fetchWeather func created to fetch the weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from "react"
 import './index.css'
 import { SearchBar } from "./components/SearchBar"
+import { compile } from "tailwindcss";
+import axios from "axios";
 
 function App () {
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+
+  const API_KEY = import .meta.env.VITE_API_KEY
+  const API_URL = `https://openweathermap.org/current`
+
+
+  const fetchWeather = async (city) => {
+    setLoading(true);
+    setError('');
+    try {
+      const url = `${API_URL}? q=${city}; `
+  }
+
+  }
 
   return (
     <div className = "min-h-screen flex flex-col items-center justify-center bg-blue-100" >
