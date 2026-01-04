cf87d3c18cba138ad89a029ec578592ac5198670
suren <surenpal666@gmail.com>
2026-01-04 17:59:30 +0900

    search bar for the UI is added and usestate is used for the JSX side

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import './index.css'
+import { SearchBar } from "./components/SearchBar"
 
 function App () {
 
 
   return (
     <div className = "min-h-screen flex flex-col items-center justify-center bg-blue-100" >
-      <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full"><h1 className = "text-3xl font-bold text-center">Weather-App</h1></div>
+      <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full"><h1 className = "text-3xl font-bold text-center mb-6">Weather-App</h1>
+      <SearchBar/>
+      </div>
     </div>
   )
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export const SearchBar = () => {
+  return (
+
+        const [city, setCity] = useState ("")
+    <form>
+        <input type="text" placeholder='Enter city name' />
+    </form>
+  )
+}
+
+
+export default SearchBar
