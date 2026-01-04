5002098691addd1802732ea0580f7cdc982137b0
suren <surenpal666@gmail.com>
2026-01-04 18:35:02 +0900

    input bar and the search bar is completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,10 +5,12 @@ export const SearchBar = () => {
     const [city, setCity] = useState ("")
 
   return (
-    <form>
+    <form className=' flex'>
         <input type="text" placeholder='Enter city name' value={city}
-        onChange={(e) => setCity = (e.target.value)}
-        className='p-2 border border-gray-300 rounded-lg outline none'/>
+        onChange={(e) => setCity (e.target.value)}
+        className='flex-1 p-2 border border-gray-300 rounded-l-lg outline none border-r-0'/>
+
+        <button className= 'bg-blue-500 border cursor-pointer p-2 hover:bg-blue-600  border-l-0 rounded-r-lg' >Search</button>
     </form>
   )
 }
