2e83f97adb2145fe451a5873f166da1d4b33860f
suren <surenpal666@gmail.com>
2026-01-04 19:23:56 +0900

    adding fetchweather to the searchbar components

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ function App () {
     } else {
       setError('Failed to fetch weather data');
     }
+    setWeather(null);
   }
 
   }
@@ -36,7 +37,7 @@ function App () {
   return (
     <div className = "min-h-screen flex flex-col items-center justify-center bg-blue-100" >
       <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full"><h1 className = "text-3xl font-bold text-center mb-6">Weather-App</h1>
-      <SearchBar/>
+      <SearchBar fetchWeather= {fetchWeather}/>
       </div>
     </div>
   )
